694a3b35eb26fcf388f3d5510295dbd40525daad
John McCann Cunniff Jr <johncunniff1248@gmail.com>
2022-09-07 22:43:36 -0400

    FIX delete

--- deleterepos.py
+++ deleterepos.py
@@ -15,16 +15,20 @@ disclude = {
 token = os.environ['GITHUB_TOKEN']
 
 def backup_delete(repo):
-    r = os.system(f'mkdir -p {repo} && git clone git@github.com:{repo}.git {repo}')
-    if r != 0:
-        return repo
-    requests.delete(
+    if repo in disclude:
+        return True
+    if not os.path.exists(repo):
+        r = os.system(f'mkdir -p {repo} && git clone git@github.com:{repo}.git {repo}')
+        if r != 0:
+            return repo
+    r = requests.delete(
         f'https://api.github.com/repos/{repo}',
         headers={
             'Accept': 'application/vnd.github+json',
             'Authorization': f'Bearer {token}',
         },
     )
+    print(r.status_code)
     return True
 
 with multiprocessing.Pool(16) as pool:
